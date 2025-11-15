4a530aa2feddd86c7a76e2079cbc30e094f95a95
Buddhike Kuruppu <buddhike.kuruppu@gmail.com>
2025-11-15 23:20:55 +1100

    update NSG module usage in dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,16 +7,30 @@ module "resource_group" {
   tags     = var.infra_tags
 }
 
+# Network Security Groups
+module "network_security_groups" {
+  source = "../../modules/network-security-group"
+
+  for_each = var.network_security_groups
+
+  name                = each.key
+  location            = var.location
+  resource_group_name = module.resource_group.name
+  security_rules      = lookup(each.value, "rules", null)
+  tags                = var.network_tags
+
+  depends_on = [module.resource_group]
+}
+
 # Virtual Network
 module "virtual_network" {
   source = "../../modules/virtual-network"
 
-  name                    = var.vnet_name
-  location                = var.location
-  resource_group_name     = module.resource_group.name
-  address_space           = var.vnet_address_space
-  network_security_groups = var.network_security_groups
-  tags                    = var.network_tags
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = module.resource_group.name
+  address_space       = var.vnet_address_space
+  tags                = var.network_tags
 
   depends_on = [module.resource_group]
 }
@@ -32,7 +46,7 @@ module "subnets" {
   virtual_network_name       = module.virtual_network.name
   address_prefixes           = each.value.address_prefixes
   delegations                = lookup(each.value, "delegations", null)
-  network_security_group_id  = lookup(each.value, "network_security_group", null) != null ? module.virtual_network.network_security_groups[each.value.network_security_group].id : null
+  network_security_group_id  = lookup(each.value, "network_security_group", null) != null ? module.network_security_groups[each.value.network_security_group].id : null
 
-  depends_on = [module.virtual_network]
+  depends_on = [module.virtual_network, module.network_security_groups]
 }
